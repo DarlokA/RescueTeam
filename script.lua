5a69fe73d5a2e56395938cbb87ef4aa279f1a7ca
Паршенцев Алексей Сергеевич <DarlokAg@gmail.com>
2021-09-19 12:07:46 +0300

    implenemt ?hire command

--- script.lua
+++ script.lua
@@ -13,6 +13,19 @@ tgt_worker_seat_name = nil;
 set_worker_id = -1;
 worker_seat_ticks = 0;
 
+none = {id = 0, name="noname"};
+worker = {id = 1, name="worker"};
+fishing = {id = 2, name="fisher"};
+waiter = {id = 3, name="waiter"};
+swimsuit = {id = 4, name="swimmer"};
+military = {id = 5, name="military"};
+office = {id = 6, name="employee"};
+police = {id = 7, name="policeman"};
+science = {id = 8, name="scientist"};
+medical = {id = 9, name="doctor"};
+wetsuit = {id = 10, name="diver"};
+civilian = {id = 11, name="citizen"};
+
 
 function onTick(game_ticks)
 
@@ -30,12 +43,64 @@ function onPlayerLeave(steam_id, name, peer_id, admin, auth)
 	
 end
 
-function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, command, one, two, three, four, five)
+function printHelp()
+	server.announce("[HELP]", "?help - show this help");
+	server.announce("[HELP]", "?hire - hire new worker in to you team.");
+end;
 
-	if (command == "?help")
-	then
-		server.announce("[HELP]", "?help - show this help");
-		server.announce("[HELP]", "?hire - hire new worker in to you team.");
-	end
+function nameFromOutFit( outfit)
+	if none.id == outFit then return none.name; end;
+	if worker.id == outFit then return worker.name; end;
+	if fishing.id == outFit then return fishing.name; end;
+	if waiter.id == outFit then return waiter.name; end;
+	if swimsuit.id == outFit then return swimsuit.name; end;
+	if military.id == outFit then return military.name; end;
+	if office.id == outFit then return office.name; end;
+	if police.id == outFit then return police.name; end;
+	if science.id == outFit then return science.name; end;
+	if medical.id == outFit then return medical.name; end;
+	if wetsuit.id == outFit then return wetsuit.name; end;
+	if civilian.id == outFit then return wetsuit.name; end;
+	return "noname";
+end;
+
+
+function outFitFromName( name )
+	if none.name == name then return none.id; end;
+	if worker.name == name then return worker.id; end;
+	if fishing.name == name then return fishing.id; end;
+	if waiter.name == name then return waiter.id; end;
+	if swimsuit.name == name then return swimsuit.id; end;
+	if military.name == name then return military.id; end;
+	if office.name == name then return office.id; end;
+	if police.name == name then return police.id; end;
+	if science.name == name then return science.id; end;
+	if medical.name == name then return medical.id; end;
+	if wetsuit.name == name then return wetsuit.id; end;
+	if civilian.name == name then return wetsuit.id; end;
+	return 0;
+end;
+
+function hire( arg1, arg2 )
+	local outfit = 1;
+	local name = "worker";
+	if (arg1 ~= nil and type(arg1) == "string") then name = arg1; outfit = outFitFromName; end;
+	if (arg1 ~= nil and type(arg1) == "number") then 
+		outfit = arg1; 
+		if arg2 ~= nil then 
+			name = arg2;
+		else
+			name = nameFromOutFit(outfit);
+		end;
+	end;
+
+
+end;
+
+
+function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, command, arg1, arg2, arg3, arg4, arg5)
+
+	if (command == "?help")	then printHelp(); end;
+	if (commmnd == "?hire") then hire(arg1); end;
 
 end
